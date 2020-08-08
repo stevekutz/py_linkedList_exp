0fee18ece9516ef8833c6eb0ab7c5546d522bef9
stevekutz <stkutz@gmail.com>
2020-08-07 22:45:06 -0500

    added delete

--- linked_list.py
+++ linked_list.py
@@ -4,6 +4,9 @@ class Node:
           self.next = None
           self.last = None
 
+     def __str__(self):
+          return str(self.value)
+
 class Linked_List:
      def __init__(self):    # head nevers holds data, not indexable, user cannot acccess
           self.head = Node() # used as placeholder to point to 1st element in LL, NOT a DATA NODE!!!
@@ -29,5 +32,62 @@ class Linked_List:
           # iterate through list until last Node located
           while current.next != None:
                total += 1
-               current = current.next
+               current = current.next # goto next node
           return total     
+
+     def print_list(self):
+          elements = []
+          current_node = self.head
+          # iterate until last node found
+          while current_node.next != None:
+               current_node = current_node.next
+               item = str(current_node.value) + "-->"
+               #elements.append(current_node.value)
+               elements.append(item)
+          print(f' elements: {elements}')
+
+
+     def get(self, index):
+          if index >= self.length() or index < 0:
+               print(f' index out of range !!') 
+               return None
+          cur_index = 0
+          current_node = self.head  # start at  Head placeholder
+          while True:
+               current_node = current_node.next  # increment
+               if cur_index == index:
+                    return current_node.value
+               cur_index += 1  # increment if not found
+
+
+     def delete(self, index):
+          if index >= self.length() or index < 0:
+               print(f' index out of range !!') 
+               return None
+          cur_index = 0
+          current_node = self.head
+          while True:
+               last_active_node = current_node   
+               current_node = current_node.next
+               if cur_index == index:
+                    # previosu active points AROUND "deleted" node to next node that exists
+                    last_active_node.next = current_node.next 
+                    return
+               cur_index += 1     
+
+
+
+
+# create instance
+new_list = Linked_List()
+
+new_list.append("First")
+new_list.append(2)
+new_list.append(3)
+new_list.print_list()   # ['First-->', '2-->', '3-->']
+print(new_list.get(2))   # 3
+
+new_list.delete(1)  # deletes value '2' at index 1
+new_list.print_list() # ['First-->', '3-->']
+
+print(new_list.length())
