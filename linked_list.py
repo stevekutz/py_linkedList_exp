4db7fc923723f3c021968246299d9e0b1e033560
stevekutz <stkutz@gmail.com>
2020-08-07 22:10:40 -0500

    added append & length

--- linked_list.py
+++ linked_list.py
@@ -1,6 +1,7 @@
 class Node:
      def __intit__(self, value = None):
-     self.value = None
-     self.next = None
-     self.last = None
+          self.value = value
+          self.next = None
+          self.last = None
 
+     
